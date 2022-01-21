946fec6b54f5a9d07a290d5b9d91e9d8be596804
jl118 <jen.liebelt@gmail.com>
2022-01-21 18:01:19 -0500

    added initial css

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -7,15 +7,17 @@ export default function Profile() {
             <div className='profile-parent'>
                 <div className='profile-details'>
                     <div className='colz'>
-                        <a href='https://github.com/jl118' target="_blank">
-                            <i className='fa fa-github'></i>
-                        </a>
-                        <a href='https://twitter.com/jenniferliebelt' target="_blank">
-                            <i className='fa fa-twitter'></i>
-                        </a>
-                        <a href='https://www.linkedin.com/in/jennifer-liebelt/' target="_blank">
-                            <i className='fa fa-linkedin'></i>
-                        </a>
+                        <div className='colz-icon'>
+                            <a href='https://github.com/jl118' target="_blank">
+                                <i className='fa fa-github'></i>
+                            </a>
+                            <a href='https://twitter.com/jenniferliebelt' target="_blank">
+                                <i className='fa fa-twitter'></i>
+                            </a>
+                            <a href='https://www.linkedin.com/in/jennifer-liebelt/' target="_blank">
+                                <i className='fa fa-linkedin'></i>
+                            </a>
+                        </div>
                     </div>
                 
                     <div className='profile-details-name'>
