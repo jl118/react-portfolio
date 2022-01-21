4e5321ac7a2231dbd10d33887cd89fe57678e2f9
jl118 <jen.liebelt@gmail.com>
2022-01-21 17:29:03 -0500

    worked on profile component

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Typical from 'react-typical';
 
 export default function Profile() {
     return (
@@ -15,6 +16,36 @@ export default function Profile() {
                         <a href='https://www.linkedin.com/in/jennifer-liebelt/' target="_blank">
                             <i className='fa fa-linkedin'></i>
                         </a>
+                    </div>
+                
+                    <div className='profile-details-name'>
+                        <span className='primary-text'>
+                            {" "}
+                            Hi! I'm <span className='highlighted-text'>Jennifer</span>
+                        </span>
+                    </div>
+                    <div className='profile-details-role'>
+                        <span className='primary-text'>
+                            {" "}
+                            <h1>
+                                {" "}
+                                <Typical 
+                                loop={Infinity}
+                                steps={[
+                                    "Full-Stack Web Developer",
+                                    1000,
+                                    "Digital Artist 🎨",
+                                    1000,
+                                    "Mario Kart Champion 🏎",
+                                    1000,
+                                    "Terrible Singer 🎤",
+                                    1000,
+                                    "Caffeine Enthusiast ☕️",
+                                    1000,
+                                ]}
+                                />
+                            </h1>
+                        </span>
                     </div>
                 </div>
             </div>
