346dca296aae6b96ddcf1decaaa07774130becb5
jl118 <jen.liebelt@gmail.com>
2022-01-21 17:01:24 -0500

    worked on profile component and added bootstrap to index.html

---
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function Profile() {
+    return (
+        <div className='profile-container'>
+            <div className='profile-parent'>
+                <div className='profile-details'>
+                    <div className='colz'>
+                        <a href='https://github.com/jl118' target="_blank">
+                            <i className='fa fa-github'></i>
+                        </a>
+                        <a href='https://twitter.com/jenniferliebelt' target="_blank">
+                            <i className='fa fa-twitter'></i>
+                        </a>
+                        <a href='https://www.linkedin.com/in/jennifer-liebelt/' target="_blank">
+                            <i className='fa fa-linkedin'></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
