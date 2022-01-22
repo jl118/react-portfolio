5b8d4ef94ff47dc2ef12e6552cb310967d6ab8a9
jl118 <jen.liebelt@gmail.com>
2022-01-21 22:32:30 -0500

    worked on About Me component

--- client/src/PortfolioContainer/AboutMe/AboutMe.js
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -4,8 +4,16 @@ import ScrollService from '../../utils/ScrollService';
 import Animations from '../../utils/Animations';
 
 export default function AboutMe(props) {
+    let fadeInScreenHandler = (screen) => {
+        if(screen.fadeScreen !== props.id)
+        return
+        Animations.animations.fadeInScreen(props.id)
+    }
+
+    const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler)
+
     return (
-        <div className='about-me-container screen-container'>
+        <div className='about-me-container screen-container' id={props.id || ""}>
             <div className='about-me-parent'>
                 <ScreenHeading title={'About Me'} subHeading={'Film to Web Development'} />
             </div>
